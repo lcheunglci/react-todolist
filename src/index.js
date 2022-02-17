1b5a78ec4898cc8f88d5dc316f07cc569cda1523
Lawrence LCI <lcheunglci@gmail.com>
2022-02-16 22:39:05 -0800

    Add data persistence to the app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import App from './App.js';
 
 import {configureStore} from './store';
 
+const store = configureStore();
+const persistor = persistStore(store);
+
 ReactDOM.render(
-    <Provider store={configureStore()}>
+    <Provider store={store}>
+      <PersistGate 
+        persistor={persistor}
+        loading={<div>Loading...</div>}
+        >
         <App />
+      </PersistGate>
     </Provider>, 
     document.getElementById('root'),
 );
