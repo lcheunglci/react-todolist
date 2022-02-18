35a31a38ab4dcc57f460cb3df821cfdbb3c4ad44
Lawrence LCI <lcheunglci@gmail.com>
2022-02-17 23:58:02 -0800

    add mock-fetch node-fetch and sinon for thunk unit test

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -12,7 +12,7 @@ export const loadTodos = () => async (dispatch, getState) => {
     try
     {
         dispatch(loadTodosInProgress());
-        const response = await fetch('http://localhost:8080/todos-delay');
+        const response = await fetch('http://localhost:8080/todos');
         const todos = await response.json();
     
         dispatch(loadTodosSuccess(todos));
